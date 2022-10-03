219f792163d44f9787df3fee03b98234ba258ddb
eunddodi <eundppojjack@gmail.com>
2022-10-04 02:35:43 +0900

    chore: set eslint&prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
   trailingComma: 'all',
+  printWidth: 120,
+  arrowParens: 'always',
 };
