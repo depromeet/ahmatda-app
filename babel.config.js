edccfb4efa2158ece086f6037eb99e62e5a297d7
eunddodi <eundppojjack@gmail.com>
2022-10-09 16:43:54 +0900

    chore: apply feedback
    - react-native-dotenv devDependency로 이동
    - 절대경로 웹 리포지토리와 통일

--- babel.config.js
+++ babel.config.js
@@ -4,20 +4,17 @@ module.exports = {
     [
       'module-resolver',
       {
-        root: ['./src'],
         extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
         alias: {
-          '~': './src',
-          '@components': './src/components',
-          '@screens': './src/screens',
-          '@assets': './src/assets',
+          '@': './src',
         },
       },
     ],
     [
       'module:react-native-dotenv',
       {
-        moduleName: 'react-native-dotenv',
+        moduleName: '@env',
+        path: '.env',
       },
     ],
   ],
