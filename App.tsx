f186cd9904fb4a4d8d444088da449335903d2f90
Hyesung Oh <haesungoh414@gmail.com>
2022-11-28 10:28:26 +0900

    chore: test log

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
+import { getUniqueId } from 'react-native-device-info';
 import { WebView } from 'react-native-webview';
 
 const BASE_URL = 'https://12-team3-web.pages.dev/';
 const App = () => {
+  useEffect(() => {
+    const getDeviceId = async () => {
+      const id = await getUniqueId();
+      // eslint-disable-next-line no-console
+      console.log(id);
+    };
+
+    getDeviceId();
+  }, []);
+
   return (
     <>
       <StatusBar />
