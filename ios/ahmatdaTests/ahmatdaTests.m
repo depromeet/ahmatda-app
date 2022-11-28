9e43d0f87fa913ea0638a9d9db62d264148e20ba
Hyesung Oh <haesungoh414@gmail.com>
2022-11-28 10:28:15 +0900

    fix: rename app

--- ios/ahmatdaTests/ahmatdaTests.m
+++ ios/ahmatdaTests/ahmatdaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface Team3AppTests : XCTestCase
+@interface ahmatdaTests : XCTestCase
 
 @end
 
-@implementation Team3AppTests
+@implementation ahmatdaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
