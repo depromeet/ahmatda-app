82346d020af1eb6dde751f12a8aacc9de1be69da
Hyesung Oh <haesungoh414@gmail.com>
2022-12-11 23:36:34 +0900

    feat: handle external link

--- App.tsx
+++ App.tsx
@@ -3,13 +3,19 @@ import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import { WebView } from 'react-native-webview';
 
+import handleNavigate from '@/utils/webViewNavigate/handleNavigate';
+
 const BASE_URL = 'https://12-team3-web.pages.dev/';
 const App = () => {
   return (
     <>
       <StatusBar />
       <SafeAreaView style={{ flex: 1 }}>
-        <WebView source={{ uri: BASE_URL }} />
+        <WebView
+          source={{ uri: BASE_URL }}
+          onNavigationStateChange={handleNavigate}
+          onShouldStartLoadWithRequest={handleNavigate}
+        />
       </SafeAreaView>
     </>
   );
