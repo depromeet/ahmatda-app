f563062ce594a4e2785745d8b088c22c86d5087d
Hyesung Oh <haesungoh414@gmail.com>
2022-12-12 00:24:38 +0900

    feat: 안드로이드 대응 로직 추가

--- src/utils/webViewNavigate/handleNavigate.ts
+++ src/utils/webViewNavigate/handleNavigate.ts
@@ -1,11 +1,21 @@
-import { Linking } from 'react-native';
+import { Linking, Platform } from 'react-native';
 import { WebViewNavigation } from 'react-native-webview';
+
+import BASE_URL from '@/constant/webView';
 
 const handleNavigate = (e: WebViewNavigation) => {
   return handleExternalLink(e);
 };
 
 const handleExternalLink = (e: WebViewNavigation) => {
+  if (Platform.OS === 'android') {
+    if (e.url.startsWith(BASE_URL)) {
+      return true;
+    }
+    Linking.openURL(e.url);
+    return false;
+  }
+
   const isExternalLink = e.navigationType === 'click';
 
   if (!isExternalLink) return true;
