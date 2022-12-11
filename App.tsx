7c62203752966908329a8be8084b0b19bce910f8
Hyesung Oh <haesungoh414@gmail.com>
2022-12-11 22:50:32 +0900

    chore: delete react-native-device-info

--- App.tsx
+++ App.tsx
@@ -1,21 +1,10 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { useEffect } from 'react';
+import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-import { getUniqueId } from 'react-native-device-info';
 import { WebView } from 'react-native-webview';
 
 const BASE_URL = 'https://12-team3-web.pages.dev/';
 const App = () => {
-  useEffect(() => {
-    const getDeviceId = async () => {
-      const id = await getUniqueId();
-      // eslint-disable-next-line no-console
-      console.log(id);
-    };
-
-    getDeviceId();
-  }, []);
-
   return (
     <>
       <StatusBar />
