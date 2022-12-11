28488470913cc44b91242a4e7aaaccbf75c924a6
Hyesung Oh <haesungoh414@gmail.com>
2022-12-12 00:57:33 +0900

    feat: 안드로이드 back button 대응

--- App.tsx
+++ App.tsx
@@ -1,15 +1,27 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { useRef } from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import { WebView } from 'react-native-webview';
 
+import useAndroidBackButton from '@/hooks/android/useAndroidBackButton';
+
 const BASE_URL = 'https://12-team3-web.pages.dev/';
 const App = () => {
+  const webViewRef = useRef<WebView | null>(null);
+
+  useAndroidBackButton(webViewRef);
+
   return (
     <>
       <StatusBar />
       <SafeAreaView style={{ flex: 1 }}>
-        <WebView source={{ uri: BASE_URL }} />
+        <WebView
+          ref={(ref) => {
+            if (!ref) return;
+            webViewRef.current = ref;
+          }}
+          source={{ uri: BASE_URL }}
+        />
       </SafeAreaView>
     </>
   );
