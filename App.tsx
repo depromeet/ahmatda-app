b3fb1bd2e5b36d8d58fb81488d6ea12a1d74d34c
hyesung oh <haesungoh414@gmail.com>
2022-12-12 18:41:09 +0900

    외부 링크 핸들링 (#15)
    * chore: delete react-native-device-info
    
    * feat: handle external link
    
    * refactor: base_url 분리
    
    * feat: 안드로이드 대응 로직 추가
    
    * chore: rename dir

--- App.tsx
+++ App.tsx
@@ -1,26 +1,21 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { useEffect } from 'react';
+import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-import { getUniqueId } from 'react-native-device-info';
 import { WebView } from 'react-native-webview';
 
-const BASE_URL = 'https://12-team3-web.pages.dev/';
+import BASE_URL from '@/constants/webView';
+import handleNavigate from '@/utils/webViewNavigate/handleNavigate';
+
 const App = () => {
-  useEffect(() => {
-    const getDeviceId = async () => {
-      const id = await getUniqueId();
-      // eslint-disable-next-line no-console
-      console.log(id);
-    };
-
-    getDeviceId();
-  }, []);
-
   return (
     <>
       <StatusBar />
       <SafeAreaView style={{ flex: 1 }}>
-        <WebView source={{ uri: BASE_URL }} />
+        <WebView
+          source={{ uri: BASE_URL }}
+          onNavigationStateChange={handleNavigate}
+          onShouldStartLoadWithRequest={handleNavigate}
+        />
       </SafeAreaView>
     </>
   );
