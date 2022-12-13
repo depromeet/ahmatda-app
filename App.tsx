576e2cb036466d7340abb8d4564754088dd767b4
eunddodi <87167786+eunddodi@users.noreply.github.com>
2022-12-13 12:42:15 +0900

    feat: 안드로이드 푸시 알림 수신 (#11)
    * chore: update pkgs
    
    * chore: rename android pakage name, applicationId
    
    * chore: remame ios bundle identifier
    
    * chore: firebase android setting
    
    * chore: install relevant packages
    
    * feat: receive notification(android)
    
    * chore: resolve conflicts
    
    * chore: install packages to resolve build error
    
    Co-authored-by: eunddodi <eundppojjack@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { useRef } from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Alert, SafeAreaView, StatusBar } from 'react-native';
 import { WebView } from 'react-native-webview';
+import messaging, { FirebaseMessagingTypes } from '@react-native-firebase/messaging';
 
 import BASE_URL from '@/constants/webView';
 import useAndroidBackButton from '@/hooks/android/useAndroidBackButton';
@@ -11,6 +12,23 @@ const App = () => {
   const webViewRef = useRef<WebView | null>(null);
 
   useAndroidBackButton(webViewRef);
+
+  useEffect(() => {
+    const getToken = async () => {
+      const token = await messaging().getToken();
+      // eslint-disable-next-line no-console
+      console.log(token);
+    };
+    getToken();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async ({ notification }: FirebaseMessagingTypes.RemoteMessage) => {
+      Alert.alert(notification?.title ?? '', notification?.body);
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <>
