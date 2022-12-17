a946c052f5fdfea9353c929e1aa76e0cc7eb6d96
kooku <koomg9599@gmail.com>
2022-12-17 20:36:27 +0900

    Ios firebase noti (#17)
    * feat: ios build success
    
    * feat: noti setting
    
    * feat: add app icon

---
+++ src/utils/firebase/messaging.ts
@@ -0,0 +1,12 @@
+import messaging from '@react-native-firebase/messaging';
+
+// eslint-disable-next-line import/prefer-default-export
+export async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED || authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+}
