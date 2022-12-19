44e6756cc74628d6ba017bde23a5937a1c8ffef6
Hyesung Oh <haesungoh414@gmail.com>
2022-12-19 12:15:30 +0900

    feat: theme 선언

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    white: '#FFFFFF',
+    gray1: '#F5F5F9',
+    gray2: '#E9E9EE',
+    gray3: '#C7C7CD',
+    gray4: '#9090A0',
+    gray5: '#626273',
+    gray6: '#464656',
+    black: '#212121',
+    primary: '#FF6C3E',
+    secondary: '#FF916F',
+    danger: '#F5656A',
+  },
+} as const;
+
+export default theme;
